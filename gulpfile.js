7c29a87471baf43125feebe102961caddec097a5
Leo Knudsen <nightskater-220@hotmail.com>
2017-01-27 15:00:29 +0100

    get boilerplate ready to use Elixir

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,7 @@
-//
+var elixir = require('laravel-elixir');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('default', ['sass', 'watch'], function() {
-	console.log('stylesheet compiled succefully');
-});
-
-gulp.task('sass', function() {
-	return gulp.src('./resources/assets/sass/app.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/css/.app.css'));
-});
-
-gulp.task('watch', ['sass'], function() {
-	gulp.watch('./app/resources/assets/sass/app.scss');
+elixir((mix) => {
+	mix.sass('app/resources/assets/sass/app.scss');
 });
